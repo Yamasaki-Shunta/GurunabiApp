9ff3ba6ca01a6c4d36a1a13f936365119ffcc7a0
Ymasaki-Sunta <syunta@gmail.com>
2020-10-31 17:38:47 +0900

    Initial Commit

---
+++ Swift6GurunabiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6GurunabiApp
+//
+//  Created by 山﨑隼汰 on 2020/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
