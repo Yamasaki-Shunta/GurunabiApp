d2ef27d62d8df0fd60b03a1d5386eb5cef4196cd
Ymasaki-Sunta <syunta@gmail.com>
2020-10-31 18:04:12 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-Swift6GurunabiApp-Swift6GurunabiAppUITests/Pods-Swift6GurunabiApp-Swift6GurunabiAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift6GurunabiApp_Swift6GurunabiAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Swift6GurunabiApp_Swift6GurunabiAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-Swift6GurunabiApp/Pods-Swift6GurunabiApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift6GurunabiApp : NSObject
+@end
+@implementation PodsDummy_Pods_Swift6GurunabiApp
+@end
